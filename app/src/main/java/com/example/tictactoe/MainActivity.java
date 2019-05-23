9406f94a320222f8696282d70b4f51919de70962
sanskardev <shikharsanskar@gmail.com>
2019-05-24 00:13:27 +0530

    Reset button added. Made it so a tile cannot change symbol.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.tictactoe;
 import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,23 +33,50 @@ public class MainActivity extends AppCompatActivity {
         }
 
         final String[] current = {"X"};
+        final boolean[][] flag = new boolean[3][3];
+        final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 final int finalI = i;
                 final int finalJ = j;
+                final int finalI1 = i;
+                final int finalJ1 = j;
                 tiles[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        tiles[finalI][finalJ].setText(current[0]);
-                        if (current[0].equals("X"))
-                            current[0] = "O";
-                        else
-                            current[0] = "X";
+                        if (flag[finalI][finalJ]) {
+                            if (Build.VERSION.SDK_INT >= 26) {
+                                vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+                            } else {
+                                vibrator.vibrate(200);
+                            }
+                        } else {
+                            tiles[finalI][finalJ].setText(current[0]);
+                            flag[finalI1][finalJ1] = true;
+                            if (current[0].equals("X"))
+                                current[0] = "O";
+                            else
+                                current[0] = "X";
+                        }
                     }
                 });
             }
         }
+
+        Button reset = findViewById(R.id.reset);
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        tiles[i][j].setText("");
+                        flag[i][j] = false;
+                    }
+                }
+            }
+        });
 
     }
 }
